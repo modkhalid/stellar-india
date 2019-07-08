8004e6644212a3d217192b87de58a980e9bf7596
mod khalid <modkhalid5@gmail.com>
2019-07-08 14:12:36 +0530

    initialise the project

---
+++ includes/header.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <!-- ANIMATE.CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css">
+
+    <!-- FONT AWESOME -->
+    <link href="./css/all.css" rel="stylesheet">
+
+    <!-- common css for every file -->
+    <link href="./css/style.css" rel="stylesheet">
+    
+
+    <title>Steller India</title>
+    
+  </head>
+  <body>
+
+
---
+++ index.php
@@ -0,0 +1,12 @@
+<!-- HEADER -->
+<?php include "./includes/header.php" ?>
+
+<!-- NAVBAR -->
+<?php include "./includes/nav.php" ?>
+
+
+
+<h1>khaid</h1>
+
+<!-- FOOTER -->
+<?php include "./includes/footer.php" ?>
